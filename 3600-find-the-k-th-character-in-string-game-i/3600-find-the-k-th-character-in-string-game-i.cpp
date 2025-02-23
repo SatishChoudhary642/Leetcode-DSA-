5dd8059179155bae4ede50d3ee35653e9a73c986
Satish <143836891+SatishChoudhary642@users.noreply.github.com>
2025-02-23 17:59:05 +0530

    Time: 1 ms (68.31%), Space: 8.2 MB (84.04%) - LeetHub

---
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        //This is for memes;
+        string s="abbcbccdbccdcddebccdcddecddedeefbccdcddecddedeefcddedeefdeefeffgbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhibccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhicddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhideefeffgeffgfggheffgfgghfgghghhieffgfgghfgghghhifgghghhighhihiij";
+        return s[k-1];
+    }
+};
