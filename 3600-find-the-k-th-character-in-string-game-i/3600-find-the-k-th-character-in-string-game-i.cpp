ef9bdfe5704909dc276dd011c88095800c8ad15e
Satish <143836891+SatishChoudhary642@users.noreply.github.com>
2025-02-23 18:08:53 +0530

    Time: 0 ms (100%), Space: 9.1 MB (70.11%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        //This is for memes;
-        string s="abbcbccdbccdcddebccdcddecddedeefbccdcddecddedeefcddedeefdeefeffgbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhibccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhicddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhideefeffgeffgfggheffgfgghfgghghhieffgfgghfgghghhifgghghhighhihiij";
+        string s="a";
+        int n=1;
+        while(n<=k){
+            int size=s.size();
+            for(int i=0; i<size; i++){
+                if(s[i]=='z'){
+                    s.push_back('a');
+                }
+                else{
+                    s.push_back(s[i]+1);
+                }
+                n++;
+            }
+        }
         return s[k-1];
     }
 };
