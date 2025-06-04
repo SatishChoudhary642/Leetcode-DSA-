17b180586100aa19c1e95ca98a845a52a619ffa5
Satish <143836891+SatishChoudhary642@users.noreply.github.com>
2025-06-04 22:16:48 +0530

    Time: 32 ms (72.76%), Space: 63.8 MB (63.43%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     string answerString(string word, int numFriends) {
-        if(numFriends==1){
+        if (numFriends == 1) {
             return word;
         }
-        int size=(word.size() -  numFriends) + 1;
-        vector<string> s;
-        char max=word[0];
 
-        for(int i=1; i<word.size(); i++){
-            if(word[i]>max){
-                max=word[i];
+        vector<string> s;
+        char max = word[0];
+
+        for (int i = 1; i < word.size(); i++) {
+            if (word[i] > max) {
+                max = word[i];
             }
         }
-        for(int i=0; i<word.size(); i++){
-            if(word[i]==max){
-                int ni=i;
-                int fl=numFriends -ni -1;
-                int j=word.size() -fl;
-                string w=word.substr(i,j-i);
+        for (int i = 0; i < word.size(); i++) {
+            if (word[i] == max) {
+                int ni = i;
+                int fl = numFriends - ni - 1;
+                int j = word.size() - fl;
+                string w = word.substr(i, j - i);
                 s.push_back(w);
             }
         }
 
-        string ans=s[0];
-        for(int i=1; i<s.size(); i++){
-            for(int j=0; j<s[i].size(); j++){
-                if(ans[j]<s[i][j]){
-                    ans=s[i];
-                    break;
-                }
-                else if(ans[j]>s[i][j]){
-                    break;
-                }
-            }
+        string ans = s[0];
+        for (int i = 1; i < s.size(); i++) {
+            if (ans < s[i]) {
+                ans = s[i];
+            } 
         }
         return ans;
     }
