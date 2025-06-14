3be39a5940b0cebf8f2f75d93c2a55fc082cc59d
Satish <143836891+SatishChoudhary642@users.noreply.github.com>
2025-06-14 20:09:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (7.17%) - LeetHub

---
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int toInt(vector<int>& v) {
+        int ans = 0, n = v.size()-1;
+        
+        if (v.size() == 1) {
+            return v[0];
+        }
+        for (int i = 0; i < v.size(); i++) {
+            int temp=v[i]*pow(10,n);
+            ans+=temp;
+            n--;
+        }
+        return ans;
+    }
+    vector<int> toVec(int n) {
+        vector<int> v;
+        if(n==0){
+            return {0};
+        }
+        while (n > 0) {
+            v.push_back(n % 10);
+            n = n / 10;
+        }
+        reverse (v.begin(), v.end());
+        return v;
+    }
+    int replace(vector<int> v, int n) {
+        int temp=v[0];
+        if(n==9){
+            for (int i = 0; i < v.size(); i++) {
+                if (v[i] != n) {
+                    temp=v[i];
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < v.size(); i++) {
+            if (v[i] == temp) {
+                v[i] = n;
+            }
+        }
+        return toInt(v);
+    }
+    int minMaxDifference(int num) {
+        vector<int> v;
+        int s, b;
+        v = toVec(num);
+        b = replace(v, 9);
+        s = replace(v, 0);
+        return b-s;
+    }
+};
