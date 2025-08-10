c325e0c59147e1290eb46403c25dc7c69e8020cf
Satish <143836891+SatishChoudhary642@users.noreply.github.com>
2025-08-10 22:33:43 +0530

    Time: 2 ms (34.81%), Space: 8.9 MB (62.83%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
-    void per_backt(int i, string& nums, vector<string>& permu) {
-        if (i == nums.size()) {
-            permu.push_back(nums);
-            return;
-        }
-        unordered_map<char, bool> visited;
-        for (int j = i; j < nums.size(); j++) {
-            if (visited.find(nums[j]) != visited.end()) {
-                continue;
-            }
-            visited[nums[j]] = true;
-            swap(nums[j], nums[i]);
-            per_backt(i + 1, nums, permu);
-            swap(nums[j], nums[i]);
-        }
-    }
     bool reorderedPowerOf2(int n) {
         if ((n & (n - 1)) == 0) {
             return true;
         }
-        vector<string> permu;
         string s = to_string(n);
-        per_backt(0, s, permu);
-        for (int i = 0; i < permu.size(); i++) {
-            int num = stoi(permu[i]);
-            if(permu[i].size()!=to_string(num).size()){
-                continue;
-            }
-            if (num > 0 && (num & (num - 1)) == 0) {
+        sort(s.begin(), s.end());
+        for (int i = 0; i < 32; i++) {
+            string num=to_string(1<<i);
+            sort(num.begin(),num.end());
+            if(s==num){
                 return true;
             }
         }
