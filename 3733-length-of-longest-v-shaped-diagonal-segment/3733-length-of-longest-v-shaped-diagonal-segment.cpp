0a3409da4745f0c672abc3c12340b77de0f04605
Satish <143836891+SatishChoudhary642@users.noreply.github.com>
2025-08-27 23:44:54 +0530

    Time: 197 ms (86.1%), Space: 103.9 MB (70.05%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> directions={{1,1},{1,-1},{-1,-1},{-1,1}};
+    int dp[500][500][4][2];
     int find(vector<vector<int>>& grid, int i, int j, int dir, bool allow, int target){
         int ni= i + directions[dir][0];
         int nj= j + directions[dir][1];
         if(ni<0 || nj<0 || ni>=grid.size() || nj>=grid[0].size() || grid[ni][nj]!=target){
             return 0;
         }
+        if(dp[ni][nj][dir][allow]!=0){
+            return dp[ni][nj][dir][allow];
+        }
         int no_turn=find(grid,ni,nj,dir,allow,2-target)+1; // if 0->2-0=2 | if 2 ->2-2=0
         int turn=0;
         if(allow){
             turn=find(grid,ni,nj,(dir+1)%4,false,2-target)+1;
         }
-        return max(no_turn,turn);
+        return dp[ni][nj][dir][allow]= max(no_turn,turn);
     }
     int lenOfVDiagonal(vector<vector<int>>& grid) {
         int ans=0, n=grid.size(), m=grid[0].size();
