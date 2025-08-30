764e5204595e913d24f61b8c728c50cd3daf3166
Satish <143836891+SatishChoudhary642@users.noreply.github.com>
2025-08-30 19:34:46 +0530

    Time: 0 ms (100%), Space: 21.6 MB (91.85%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         for (int i = 0; i < 9; i++) {
-            vector<int> row(10), col(10);
+            bool row[10] = {false};
+            bool col[10] = {false};
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.') {
-                    row[board[i][j] - '0']++;
-                    if (row[board[i][j] - '0'] >= 2)
+                    if (row[board[i][j] - '0'])
                         return false;
+                    row[board[i][j] - '0'] = true;
                 }
                 if (board[j][i] != '.') {
-                    col[board[j][i] - '0']++;
-                    if (col[board[j][i] - '0'] >= 2)
+                    if (col[board[j][i] - '0'])
                         return false;
+                    col[board[j][i] - '0'] = true;
                 }
             }
         }
-        for (int i = 0; i < 9; i+=3) {
-            for (int j = 0; j < 9; j+=3) {
-                vector<int> num(10);
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                bool num[10] = {false};
                 for (int r = i; r < i + 3; r++) {
                     for (int c = j; c < j + 3; c++) {
-                        if (board[r][c] != '.') {
-                            num[board[r][c] - '0']++;
-                            if (num[board[r][c] - '0'] >= 2)
-                                return false;
-                        }
+                        if (board[r][c] == '.')
+                            continue;
+                        if (num[board[r][c] - '0'])
+                            return false;
+                        num[board[r][c] - '0'] = true;
                     }
                 }
             }
