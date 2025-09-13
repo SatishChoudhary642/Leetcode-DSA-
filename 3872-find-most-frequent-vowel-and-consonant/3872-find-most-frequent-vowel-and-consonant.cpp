d31940999850c8d7b7091066e87183f2baba941a
Satish <143836891+SatishChoudhary642@users.noreply.github.com>
2025-09-13 23:14:42 +0530

    Time: 0 ms (100%), Space: 9.8 MB (44.54%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        unordered_map<char,int> m;
-        for (auto ch : s) m[ch]++;
-        int ccnt=0, vcnt=0;
-        for (auto [ch,cnt] : m) {
+        unordered_map<char, int> m;
+        int ccnt = 0, vcnt = 0;
+        for (auto ch : s) {
+            int cnt=++m[ch];
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                vcnt=max(vcnt,cnt);
-            }
-            else{
-                ccnt=max(ccnt,cnt);
+                vcnt = max(vcnt, cnt);
+            } else {
+                ccnt = max(ccnt, cnt);
             }
         }
+
         return vcnt + ccnt;
     }
 };
